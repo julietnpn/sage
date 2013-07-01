c26c4a9c464cdbafe0d23212d1367b8872c53335
Sahand Nayebaziz <thesahand@gmail.com>
2013-07-01 13:07:52 -0700

    add drawing to the map and improved the overlays
    - drawing libraries imported on load
    - overlays moved to center top
    - unused drawing tools no longer loaded

--- composer/composer mark IV/aloe.js
+++ composer/composer mark IV/aloe.js
@@ -1,10 +1,39 @@
 function startMap() {
-	var mapOptions = {
-		center: new google.maps.LatLng(33.640133,-117.840165),
-        zoom: 13,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    }
-    google.maps.event.addDomListener(window, 'load', startMap);
+  var mapOptions = {
+    center: new google.maps.LatLng(33.640133,-117.840165),
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
 
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [
+        google.maps.drawing.OverlayType.POLYGON,
+//      google.maps.drawing.OverlayType.CIRCLE,
+        google.maps.drawing.OverlayType.MARKER,
+//      google.maps.drawing.OverlayType.POLYLINE,
+//        //google.maps.drawing.OverlayType.RECTANGLE
+      ]
+    },
+    markerOptions: {
+//      icon: 'images/beachflag.png'
+    },
+    circleOptions: {
+      fillColor: '#ffff00',
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1
+    }
+  });
+  drawingManager.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', startMap);
