d758f2903136f85fc59d70021a9011c2bd996a19
ksegismundo <ksegismu@uci.edu>
2016-01-20 13:41:43 -0800

    new file:   templates/frontend/cardview.html 	new file:   templates/frontend/frontpage.html 	modified:   templates/frontend/index.html 	modified:   templates/frontend/sidebar.html 	modified:   views.py

--- database/plantsproject/frontend/views.py
+++ database/plantsproject/frontend/views.py
@@ -1,15 +1,11 @@
-#from django.http import HttpResponse
-#from django.template import loader
 from django.shortcuts import render
 
 from .models import Plant
 
 def index(request):	
-	plant_list = Plant.objects.all()
-	#template = loader.get_template('frontend/index.html')
+	plant_list = Plant.objects.filter(genus__startswith='Ag')
 	context = {
 		'plant_list': plant_list,
 	}
 
-	return render(request, 'frontend/index.html', context)
-	#return HttpResponse(template.render(context, request))
+	return render(request, 'frontend/cardview.html', context)
