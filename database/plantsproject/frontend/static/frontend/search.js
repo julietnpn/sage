62dc1d6a4d1aa880d88698b1712ad3d63f009556
julietnpn <pacificsage@Pacific-Sage.local>
2016-06-26 08:47:45 -0700

    Removed database directory after name change.

--- database/plantsproject/frontend/static/frontend/search.js
+++
@@ -1,7 +0,0 @@
-$("#search-magnifying-glass").on("click", function(){
-	$('#searchform').submit();
-});
-
-$("#searchform").submit(function(){
-	$("#searchform").attr("action", "/search/" + $("#searchbar").val() + "/");
-});
