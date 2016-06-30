fd1aaeb0510f3129a3542e97c8facf1ac761a1ef
julietnpn <julietnpn@gmail.com>
2016-06-30 12:27:57 -0700

    Created hello world for the composer.

--- djangoproject/SAGEproject/plantsproject/urls.py
+++ djangoproject/SAGEproject/plantsproject/urls.py
@@ -30,4 +30,5 @@ urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page':'/'}, name='logout'),
     url(r'^', include('frontend.urls')),
+    url(r'^composer/', include('composer.urls')),
 )
