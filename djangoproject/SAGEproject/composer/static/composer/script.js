b9775ae5eac7f0be894c93ffb7217b8c936d9351
Daniel McInnis <danmcinnis@cox.net>
2016-11-28 12:19:55 -0800

    Time and money page form inputs are captured and serialized

--- djangoproject/SAGEproject/composer/static/composer/script.js
+++ djangoproject/SAGEproject/composer/static/composer/script.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
-	$("#time-box-button").click(function(){
-		console.log("test");
-		$(".time-box").append('<div class="row add-margin-top-sm">\
-			<div class="col-xs-4 col-xs-offset-4 text-center form-inline">\
-				<select class="form-control" name="time-number" value="Time Amount">\
-					<option value="" disabled selected>Amount</option>\
-					<option value="1">1 hour</option>\
-					<option value="2">2 hours</option>\
-					<option value="3">3 hours</option>\
-					<option value="4">4 hours</option>\
-					<option value="5">5 hours</option>\
-					<option value="6">6 hours</option>\
-					<option value="7">7 hours</option>\
-					<option value="8">8 hours</option>\
-					<option value="9">9 hours</option>\
-					<option value="10">10 hours</option>\
-				</select>\
-				<span>per</span>\
-				<select class="form-control" name="time-number" value="Time Unit">\
-					<option value="" disabled selected>Unit</option>\
-					<option value="1">day</option>\
-					<option value="2">week</option>\
-					<option value="3">month</option>\
-				</select>\
-			</div>\
-		</div>');
-	});
+	// Better to have all three options on the page already and remove some complexity, or allow the user to add / subtract which options they'd like to have when it comes to choosing time?
+	// $("#time-box-button").click(function(){
+	// 	$(".time-box").append('<div class="row add-margin-top-sm">\
+	// 		<div class="col-xs-4 col-xs-offset-4 text-center form-inline">\
+	// 			<select class="form-control" name="time-number" value="Time Amount">\
+	// 				<option value="" disabled selected>Amount</option>\
+	// 				<option value="1">1 hour</option>\
+	// 				<option value="2">2 hours</option>\
+	// 				<option value="3">3 hours</option>\
+	// 				<option value="4">4 hours</option>\
+	// 				<option value="5">5 hours</option>\
+	// 				<option value="6">6 hours</option>\
+	// 				<option value="7">7 hours</option>\
+	// 				<option value="8">8 hours</option>\
+	// 				<option value="9">9 hours</option>\
+	// 				<option value="10">10 hours</option>\
+	// 			</select>\
+	// 			<span>per</span>\
+	// 			<select class="form-control" name="time-number" value="Time Unit">\
+	// 				<option value="" disabled selected>Unit</option>\
+	// 				<option value="1">day</option>\
+	// 				<option value="2">week</option>\
+	// 				<option value="3">month</option>\
+	// 			</select>\
+	// 		</div>\
+	// 	</div>');
+	// });
 
 	$("#to-goalchart-button").click(function(){
 		var productsSerialzed = $("#products-form").serialize();
@@ -38,12 +38,13 @@ $(document).ready(function(){
 		$.post("/composer/", ecosystemSerialzed);
 	});
 
-	// $("#to-constraints-button").click(function(){
-	// 	var productsSerialzed = $("#time-form").serialize();
-	// 	$.post("/composer/", productsSerialzed);
-	// 	$.post("/composer/", servicesSerialzed);
-	// 	$.post("/composer/", wildlifeSerialzed);
-	// 	$.post("/composer/", ecosystemSerialzed);
-	// });
+	$("#to-constraints-button").click(function(){
+		var moneySerialized = $("#money-form").serialize();
+		var timeSerialized = $("#time-form").serialize();
+		// console.log(moneySerialized);
+		// console.log(timeSerialized);
+		// $.post("/composer/", moneySerialized);
+		// $.post("/composer/", timeSerialized);
+	});
 
 });
