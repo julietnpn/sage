15237d65ada29351c96b8832cd864e61ba90833a
Jiachen Huang <jiacheh4@uci.edu>
2018-07-09 11:48:23 -0700

    added additional text in about page

--- plantsproject/settings.py
+++ plantsproject/settings.py
@@ -93,7 +93,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'plants',
+        'NAME': 'django-plants',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': 'localhost',
